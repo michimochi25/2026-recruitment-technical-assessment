b4161f50f7a6c4f0c8d617d753dbd8a4e815155b
Giselle Angela Hanjaya Putri <z5491414@ad.unsw.edu.au>
2026-02-17 22:14:39 +1100

    add bottom margin

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -35,7 +35,7 @@ function App() {
         )}
 
         {/* Rooms */}
-        <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-4 mt-32 sm:mt-20">
+        <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-4 mt-32 sm:mt-20 mb-10">
           {rooms.map((room) => (
             <>
               {isMobile ? (
